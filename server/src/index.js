af33bc8f5ec785abdf7bae7ce797354e5af5ce4f
rares <rares21m@gmail.com>
2026-07-07 20:34:27 +0300

    Fix HTTPS proxy handling for BRD OAuth

--- server/src/index.js
+++ server/src/index.js
@@ -27,6 +27,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.set("trust proxy", 1);
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
